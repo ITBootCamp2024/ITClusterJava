9b13d572f4704f1768503d345a99cb537db4c3ef
Andrey Kahamlyk <kagamlykas@gmail.com>
2024-03-19 20:54:23 +0200

    Added entity and repo for course and programs level

---
+++ src/main/java/com/ua/itclusterjava2024/entity/ProgramsLevel.java
@@ -0,0 +1,21 @@
+package com.ua.itclusterjava2024.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "programs_level")
+public class ProgramsLevel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
