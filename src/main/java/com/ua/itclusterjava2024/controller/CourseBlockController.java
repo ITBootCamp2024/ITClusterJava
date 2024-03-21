3767b1cdba8aeed028f567f063d80a0a39c4a659
Daria Maier <daria.maier0104@gmail.com>
2024-03-21 12:55:37 +0200

    added CorsConfig for Cors error. Not final version

--- src/main/java/com/ua/itclusterjava2024/controller/CourseBlockController.java
+++ src/main/java/com/ua/itclusterjava2024/controller/CourseBlockController.java
@@ -1,10 +1,7 @@
 package com.ua.itclusterjava2024.controller;
 
 import com.ua.itclusterjava2024.entity.CourseBlock;
-import com.ua.itclusterjava2024.service.implementation.CourseBlockServiceImpl;
 import com.ua.itclusterjava2024.service.interfaces.CourseBlockService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,30 +14,28 @@ public class CourseBlockController {
     public CourseBlockController(CourseBlockService courseBlockService) {
         this.courseBlockService = courseBlockService;
     }
-    @GetMapping("/course_blocks")
+    @GetMapping
     public List<CourseBlock> showCourseBlockList(){
         return courseBlockService.getAll();
     }
 
-    // Need to complete
-    @PostMapping("/course_blocks/save")
+    @PostMapping
     public CourseBlock saveCourseBlock(@RequestBody CourseBlock courseBlock){
         return courseBlockService.create(courseBlock);
     }
 
-    // Need to complete
-    @PutMapping("/course_blocks/{id}")
+    @PutMapping("/{id}")
     public CourseBlock updateCourseBlock(//@PathVariable Long id,
                                          @RequestBody CourseBlock courseBlock){
         return courseBlockService.update(courseBlock);
     }
 
-    @GetMapping("/course_blocks/{id}")
+    @GetMapping("/{id}")
     public CourseBlock findById(@PathVariable Long id){
         return courseBlockService.readById(id);
     }
 
-    @DeleteMapping("/course_blocks/delete/{id}")
+    @DeleteMapping("/{id}")
     public String deleteCourseBlock(@PathVariable Long id){
         courseBlockService.delete(id);
         return "Successfully deleted";
