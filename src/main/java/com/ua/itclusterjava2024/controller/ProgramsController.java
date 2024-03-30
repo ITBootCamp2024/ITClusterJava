259799151943f4375eeac80b37c228716c9cd41d
Daria Maier <daria.maier0104@gmail.com>
2024-03-30 10:28:20 +0200

    update cors

--- src/main/java/com/ua/itclusterjava2024/controller/ProgramsController.java
+++ src/main/java/com/ua/itclusterjava2024/controller/ProgramsController.java
@@ -3,12 +3,18 @@ package com.ua.itclusterjava2024.controller;
 import com.ua.itclusterjava2024.dto.ProgramsDTO;
 import com.ua.itclusterjava2024.entity.Programs;
 import com.ua.itclusterjava2024.service.interfaces.ProgramsService;
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
