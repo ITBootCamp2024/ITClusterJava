a30cff6016a625dc1530153c3781f8a48df6b2ea
kllymenko <alexklimovv2@gmail.com>
2024-04-08 14:31:55 +0300

    ServiceInfoDTO + PageWrapper changes

--- src/main/java/com/ua/itclusterjava2024/dto/ServiceInfoDTO.java
+++ src/main/java/com/ua/itclusterjava2024/dto/ServiceInfoDTO.java
@@ -1,5 +1,6 @@
 package com.ua.itclusterjava2024.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,12 +13,27 @@ import java.util.List;
 @Data
 @Builder
 public class ServiceInfoDTO {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<PositionDTO> position;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<EducationLevelDTO> education_levels;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<TeachersDTO> teachers;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<UniversityDTO> university;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<SpecialtyDTO> specialty;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<EducationProgramsDTO> education_program;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<DisciplinesDTO> discipline;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<DisciplineBlocksDTO> disciplineBlocks;
 }
--- src/main/java/com/ua/itclusterjava2024/wrappers/PageWrapper.java
+++ src/main/java/com/ua/itclusterjava2024/wrappers/PageWrapper.java
@@ -1,5 +1,6 @@
 package com.ua.itclusterjava2024.wrappers;
 
+import com.ua.itclusterjava2024.dto.ServiceInfoDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import java.util.List;
 @Data
 public class PageWrapper<T> {
     private List<T> content;
+    private ServiceInfoDTO service_info;
     private long totalElements;
 }
 
