a4c80aae24f6bd3851b277520724b425b497f595
kllymenko <alexklimovv2@gmail.com>
2024-04-08 15:26:01 +0300

    DepartmentController findAll method done

--- src/main/java/com/ua/itclusterjava2024/wrappers/PageWrapper.java
+++ src/main/java/com/ua/itclusterjava2024/wrappers/PageWrapper.java
@@ -1,5 +1,6 @@
 package com.ua.itclusterjava2024.wrappers;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.ua.itclusterjava2024.dto.ServiceInfoDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,7 +13,10 @@ import java.util.List;
 @Data
 public class PageWrapper<T> {
     private List<T> content;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private ServiceInfoDTO service_info;
+
     private long totalElements;
 }
 
