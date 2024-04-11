e63cbd23baf3f7a66f85d575e3d42548adf6ef6f
Oryna Kasapova <oryna.kasapova@nure.ua>
2024-04-11 18:10:10 +0300

    discipline blocks, discipline, discipline group, teachers refactoring

--- src/main/java/com/ua/itclusterjava2024/controller/DisciplineGroupController.java
+++ src/main/java/com/ua/itclusterjava2024/controller/DisciplineGroupController.java
@@ -9,10 +9,9 @@ import com.ua.itclusterjava2024.service.interfaces.DisciplineGroupService;
 import com.ua.itclusterjava2024.validators.CourseGroupValidator;
 import com.ua.itclusterjava2024.wrappers.PageWrapper;
 import com.ua.itclusterjava2024.wrappers.Patcher;
+import jakarta.persistence.EntityManager;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,14 +24,17 @@ public class DisciplineGroupController {
     private final DisciplineBlocksService disciplineBlocksService;
     private final ModelMapper modelMapper;
     private final CourseGroupValidator courseGroupValidator;
-    private final Patcher patcher;
+    private final Patcher<DisciplineGroups> patcher;
+    private final EntityManager entityManager;
 
-    public DisciplineGroupController(DisciplineGroupService disciplineGroupService, DisciplineBlocksService disciplineBlocksService, ModelMapper modelMapper, CourseGroupValidator courseGroupValidator, Patcher patcher) {
+
+    public DisciplineGroupController(DisciplineGroupService disciplineGroupService, DisciplineBlocksService disciplineBlocksService, ModelMapper modelMapper, CourseGroupValidator courseGroupValidator, Patcher patcher, EntityManager entityManager) {
         this.disciplineGroupService = disciplineGroupService;
         this.disciplineBlocksService = disciplineBlocksService;
         this.modelMapper = modelMapper;
         this.courseGroupValidator = courseGroupValidator;
         this.patcher = patcher;
+        this.entityManager = entityManager;
     }
 
     @GetMapping
@@ -52,48 +54,51 @@ public class DisciplineGroupController {
     public PageWrapper<DisciplineGroupsDTO> save(@RequestBody @Valid DisciplineGroupsDTO disciplineGroupsDTO,
                                                  BindingResult bindingResult) {
         courseGroupValidator.validate(disciplineGroupsDTO, bindingResult);
-        if (bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             throw new ValidationException(bindingResult);
         }
         disciplineGroupService.create(convertToEntity(disciplineGroupsDTO));
+        entityManager.clear();
         return findAll();
     }
 
     @CrossOrigin
     @PatchMapping("/{id}")
-    public PageWrapper<DisciplineGroupsDTO> update(@PathVariable Long id, @RequestBody DisciplineGroups updatedDisciplineGroups) {
+    public PageWrapper<DisciplineGroupsDTO> update(@PathVariable("id") Long id,
+                                                   @RequestBody DisciplineGroupsDTO disciplineGroupsDTO) {
         DisciplineGroups existingDisciplineGroups = disciplineGroupService.readById(id)
                 .orElseThrow(() -> new NotFoundException("DisciplineGroups not found with id: " + id));
+        DisciplineGroups updatedDisciplineGroupsDTO = convertToEntity(disciplineGroupsDTO);
         try {
-            patcher.patch(existingDisciplineGroups, updatedDisciplineGroups);
+            patcher.patch(existingDisciplineGroups, updatedDisciplineGroupsDTO);
+            disciplineGroupService.update(id, existingDisciplineGroups);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
-
-        disciplineGroupService.update(id, existingDisciplineGroups);
+        entityManager.clear();
         return findAll();
     }
 
     @GetMapping("/{id}")
-    public DisciplineGroupsDTO findById(@PathVariable Long id){
+    public DisciplineGroupsDTO findById(@PathVariable Long id) {
         return convertToDTO(disciplineGroupService.readById(id).orElse(null));
     }
 
     @DeleteMapping("/{id}")
-    public PageWrapper<DisciplineGroupsDTO> delete(@PathVariable Long id){
+    public PageWrapper<DisciplineGroupsDTO> delete(@PathVariable Long id) {
         disciplineGroupService.delete(id);
         return findAll();
     }
 
-    private DisciplineGroups convertToEntity(DisciplineGroupsDTO disciplineGroupsDTO){
+    private DisciplineGroups convertToEntity(DisciplineGroupsDTO disciplineGroupsDTO) {
         DisciplineGroups disciplineGroups = modelMapper.map(disciplineGroupsDTO, DisciplineGroups.class);
-        DisciplineBlocks disciplineBlocks = disciplineBlocksService.readById(disciplineGroupsDTO.getBlock().getId())
-                .orElseThrow(() -> new NotFoundException("DisciplineBlocks not found"));
-        disciplineGroups.setBlock_id(disciplineBlocks);
+        if (disciplineGroupsDTO.getBlock() != null) {
+            disciplineGroups.setBlock_id(modelMapper.map(disciplineGroupsDTO.getBlock(), DisciplineBlocks.class));
+        }
         return disciplineGroups;
     }
 
-    private DisciplineGroupsDTO convertToDTO(DisciplineGroups disciplineGroups){
+    private DisciplineGroupsDTO convertToDTO(DisciplineGroups disciplineGroups) {
         DisciplineGroupsDTO dto = modelMapper.map(disciplineGroups, DisciplineGroupsDTO.class);
         dto.setBlock(DisciplineBlocksDTO.builder()
                 .id(disciplineGroups.getBlock_id().getId())
