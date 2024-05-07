ffc55bc5fee32fc5a832f7265791669f99bf8260
Oksana Vykhryst <oksana.vykhryst@nure.ua>
2024-05-07 19:05:27 +0300

    removed unused code

--- src/main/java/com/ua/itclusterjava2024/controller/DisciplineGroupController.java
+++ src/main/java/com/ua/itclusterjava2024/controller/DisciplineGroupController.java
@@ -12,8 +12,6 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
