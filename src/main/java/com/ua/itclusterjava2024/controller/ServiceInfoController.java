1658cee924b410c162a59e7d8a4438dbffd7b043
Oksana Vykhryst <oksana.vykhryst@nure.ua>
2024-05-07 18:29:56 +0300

    ServiceInfoController refactored

--- src/main/java/com/ua/itclusterjava2024/controller/ServiceInfoController.java
+++ src/main/java/com/ua/itclusterjava2024/controller/ServiceInfoController.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,6 +21,7 @@ import java.util.List;
 public class ServiceInfoController {
     private final ServiceInfoService serviceInfoService;
     private final ModelMapper modelMapper;
+    private static final int PAGE_SIZE = 20;
 
     @Autowired
     public ServiceInfoController(ServiceInfoService serviceInfoService, ModelMapper modelMapper) {
@@ -31,62 +31,88 @@ public class ServiceInfoController {
 
     @GetMapping
     public PageWrapper<ServiceInfoDTO> findAll(@RequestParam(defaultValue = "1") int page) {
-        int pageSize = 20;
-        PageRequest pageable = PageRequest.of(page - 1, pageSize);
-
+        PageRequest pageable = PageRequest.of(page - 1, PAGE_SIZE);
         Page<ServiceInfoDTO> pageOfServiceInfoDTO = createPage(pageable);
-
-        PageWrapper<ServiceInfoDTO> pageWrapper = new PageWrapper<>();
-        pageWrapper.setContent(pageOfServiceInfoDTO.getContent());
-        pageWrapper.setTotalElements(pageOfServiceInfoDTO.getTotalElements());
-        return pageWrapper;
+        return new PageWrapper<>(pageOfServiceInfoDTO.getContent(), pageOfServiceInfoDTO.getTotalElements());
     }
 
     private Page<ServiceInfoDTO> createPage(Pageable pageable) {
-        List<UniversityDTO> universitiesDTO = new ArrayList<>();
-
-        List<University> universities = serviceInfoService.getAllUniversities();
-        universities.forEach(university -> {
-            List<Department> departments = serviceInfoService.getAllDepartmentsByUniversityId(university.getId());
-            List<DepartmentDTO> departmentsDTO = departments.stream()
-                    .map(department -> DepartmentDTO.builder().id(department.getId()).name(department.getName()).build())
-                    .toList();
-
-            universitiesDTO.add(UniversityDTO.builder().id(university.getId()).name(university.getName()).abbr(university.getAbbr()).department(departmentsDTO).build());
-        });
-
-        List<DisciplineBlocksDTO> disciplineBlocksDTO = new ArrayList<>();
-        List<DisciplineBlocks> disciplineBlocks = serviceInfoService.getAllDisciplineBlocks();
-        disciplineBlocks.forEach(disciplineBlock -> {
-            List<DisciplineGroups> disciplineGroup = serviceInfoService.getAllDisciplineGroupsByDisciplineBlocksId(disciplineBlock);
-            List<DisciplineGroupsDTO> disciplineGroupsDTO = disciplineGroup.stream()
-                    .map(disciplineGroups -> DisciplineGroupsDTO.builder().id(disciplineGroups.getId()).name(disciplineGroups.getName()).build())
-                    .toList();
-
-            disciplineBlocksDTO.add(DisciplineBlocksDTO.builder().id(disciplineBlock.getId()).name(disciplineBlock.getName()).disciplineGroups(disciplineGroupsDTO).build());
-        });
-
-        ServiceInfoDTO serviceInfoDTO = ServiceInfoDTO.builder()
-                .position(serviceInfoService.getAllPositions().stream().map(this::convertPositionToDTO).toList())
-                .educationLevels(serviceInfoService.getAllEducationLevels().stream().map(this::convertEducationLevelToDTO).toList())
-                .teachers(serviceInfoService.getAllTeachers().stream().map(teachers -> TeachersDTO.builder().id(teachers.getId()).name(teachers.getName()).build()).toList())
-                .university(universitiesDTO)
-                .specialty(serviceInfoService.getAllSpecialties().stream().map(specialty -> SpecialtyDTO.builder().id(specialty.getId()).code(specialty.getCode()).name(specialty.getName()).build()).toList())
-                .educationProgram(serviceInfoService.getAllEducationPrograms().stream().map(educationPrograms -> EducationProgramsDTO.builder().id(educationPrograms.getId()).name(educationPrograms.getName()).programUrl(educationPrograms.getProgramUrl()).build()).toList())
-                .discipline(serviceInfoService.getAllDisciplines().stream().map(disciplines -> DisciplinesDTO.builder().id(disciplines.getId()).name(disciplines.getName()).build()).toList())
-                .disciplineBlocks(disciplineBlocksDTO)
-                .build();
-
+        ServiceInfoDTO serviceInfoDTO = buildServiceInfoDTO();
         List<ServiceInfoDTO> content = Collections.singletonList(serviceInfoDTO);
         return new PageImpl<>(content, pageable, content.size());
     }
 
-    private PositionDTO convertPositionToDTO(Position position){
+    private ServiceInfoDTO buildServiceInfoDTO() {
+        return ServiceInfoDTO.builder()
+                .position(serviceInfoService.getAllPositions().stream().map(this::convertPositionToDTO).toList())
+                .educationLevels(serviceInfoService.getAllEducationLevels().stream().map(this::convertEducationLevelToDTO).toList())
+                .teachers(serviceInfoService.getAllTeachers().stream().map(this::convertTeacherToDTO).toList())
+                .university(buildUniversityDTO())
+                .specialty(serviceInfoService.getAllSpecialties().stream().map(this::convertSpecialtyToDTO).toList())
+                .educationProgram(serviceInfoService.getAllEducationPrograms().stream().map(this::convertEducationProgramToDTO).toList())
+                .discipline(serviceInfoService.getAllDisciplines().stream().map(this::convertDisciplineToDTO).toList())
+                .disciplineBlocks(buildDisciplineBlocksDTO())
+                .build();
+    }
+
+    private List<UniversityDTO> buildUniversityDTO() {
+        return serviceInfoService.getAllUniversities().stream()
+                .map(this::convertUniversityToDTO)
+                .toList();
+    }
+
+    private List<DisciplineBlocksDTO> buildDisciplineBlocksDTO() {
+        return serviceInfoService.getAllDisciplineBlocks().stream()
+                .map(this::convertDisciplineBlockToDTO)
+                .toList();
+    }
+
+    private PositionDTO convertPositionToDTO(Position position) {
         return modelMapper.map(position, PositionDTO.class);
     }
 
-    private EducationLevelDTO convertEducationLevelToDTO(EducationLevel position){
-        return modelMapper.map(position, EducationLevelDTO.class);
+    private EducationLevelDTO convertEducationLevelToDTO(EducationLevel educationLevel) {
+        return modelMapper.map(educationLevel, EducationLevelDTO.class);
     }
 
+    private TeachersDTO convertTeacherToDTO(Teachers teacher) {
+        return TeachersDTO.builder().id(teacher.getId()).name(teacher.getName()).build();
+    }
+
+    private UniversityDTO convertUniversityToDTO(University university) {
+        List<DepartmentDTO> departmentsDTO = serviceInfoService.getAllDepartmentsByUniversityId(university.getId()).stream()
+                .map(department -> DepartmentDTO.builder().id(department.getId()).name(department.getName()).build())
+                .toList();
+
+        return UniversityDTO.builder()
+                .id(university.getId())
+                .name(university.getName())
+                .abbr(university.getAbbr())
+                .department(departmentsDTO)
+                .build();
+    }
+
+    private SpecialtyDTO convertSpecialtyToDTO(Specialty specialty) {
+        return SpecialtyDTO.builder().id(specialty.getId()).code(specialty.getCode()).name(specialty.getName()).build();
+    }
+
+    private EducationProgramsDTO convertEducationProgramToDTO(EducationPrograms educationPrograms) {
+        return EducationProgramsDTO.builder().id(educationPrograms.getId()).name(educationPrograms.getName()).programUrl(educationPrograms.getProgramUrl()).build();
+    }
+
+    private DisciplinesDTO convertDisciplineToDTO(Disciplines discipline) {
+        return DisciplinesDTO.builder().id(discipline.getId()).name(discipline.getName()).build();
+    }
+
+    private DisciplineBlocksDTO convertDisciplineBlockToDTO(DisciplineBlocks disciplineBlock) {
+        List<DisciplineGroupsDTO> disciplineGroupsDTO = serviceInfoService.getAllDisciplineGroupsByDisciplineBlocksId(disciplineBlock).stream()
+                .map(disciplineGroups -> DisciplineGroupsDTO.builder().id(disciplineGroups.getId()).name(disciplineGroups.getName()).build())
+                .toList();
+
+        return DisciplineBlocksDTO.builder()
+                .id(disciplineBlock.getId())
+                .name(disciplineBlock.getName())
+                .disciplineGroups(disciplineGroupsDTO)
+                .build();
+    }
 }
